14c237b6d2b7ec165b40f91bc70f1610a5327b77
urielbp <urielb@gmail.com>
2019-03-22 16:33:03 +0100

    Initial Commit

---
+++ Prototipo QR/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Prototipo QR
+//
+//  Created by Uriel Pinheiro on 22/03/2019.
+//  Copyright © 2019 Uriel Pinheiro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
